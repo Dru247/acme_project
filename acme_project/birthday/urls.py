155c2717c56a36a676f6fd96ef700e741784a79b
Dru <lehtin.aa@mail.ru>
2025-06-11 20:49:31 +0300

    Добавлено сохранение данных из формы в БД.

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from . import views
+from .views import birthday, birthday_list
 
 app_name = 'birthday'
 
 urlpatterns = [
-    path('', views.birthday, name='create'),
+    path('', birthday, name='create'),
+    path('list/', birthday_list, name='list')
 ]
